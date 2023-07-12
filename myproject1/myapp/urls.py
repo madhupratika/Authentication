45488e7fbd4bad7d658ec9f4d609b831ed4a9821
MadhuPratika <madhusandy1323@gmail.com>
2023-07-12 11:43:36 +0530

    final

--- myproject1/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from myapp import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('hello/', views.hello, name='hello'),
-]
